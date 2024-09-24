d4913474220e5804060245f0ebcf24ce357ece99
kutail <kutep0v.vlad.m@gmail.com>
2024-09-24 13:24:47 +0500

    fixwd

--- 24.09.2024/main.cpp
+++ 24.09.2024/main.cpp
@@ -3,10 +3,13 @@
 int main()
 {
     int c = 0;
+    int sum = 0;
     while (c!=1)
     {
         std::cout << "Vvedite chislo\n";
         std::cin >> c;
+        sum+=c;
     }
+    std::cout << "Summa=" << sum;
     return 0;
 }
